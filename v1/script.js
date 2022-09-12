7b29f19a70a208113cd2d6b03a26b62bbbc8cc7e
yoursamlan <skamlan99@gmail.com>
2022-09-13 01:52:04 +0530

    new redir instruction

--- v1/script.js
+++ v1/script.js
@@ -1,8 +1,8 @@
-function scale(){
+function scale() {
     var scale = 'scale(1)';
-document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
- document.body.style.msTransform =   scale;       // IE 9
- document.body.style.transform = scale; 
+    document.body.style.webkitTransform = scale; // Chrome, Opera, Safari
+    document.body.style.msTransform = scale; // IE 9
+    document.body.style.transform = scale;
 }
 
 function shuffle(array) {
@@ -20,24 +20,27 @@ function shuffle(array) {
 
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  
+}
+
 function reA(arr, value) {
-    if (isNaN(value) || value > x["totalPost"]){
-        value = randInt(1,x["totalPost"]);
+    if (isNaN(value) || value > x["totalPost"]) {
+        value = randInt(1, x["totalPost"]);
     }
     var index = arr.indexOf(value);
     if (index > -1) {
-      arr.splice(index, 1);
+        arr.splice(index, 1);
     }
     return [value].concat(arr);
-  }
+}
 
-var postArr=Array.from({length:x.totalPost},(_,a)=>a+1);
+var postArr = Array.from({
+    length: x.totalPost
+}, (_, a) => a + 1);
+
 function postList() {
     arr = shuffle(postArr);
     postNum = parseInt(window.location.href.split("?")[1]);
-    arr = reA(arr,postNum);
+    arr = reA(arr, postNum);
     window.postArr = arr;
 }
 
@@ -61,14 +64,14 @@ function createPost() {
         post.setAttribute("class", "mBlog");
         bgwidth = 750 + i;
         post.setAttribute("style", "background-image: url('https://source.unsplash.com/featured/400x" + bgwidth + "');");
-        if (x["showAuth"]){
-            post.innerHTML = `<div class="blogPost"><p class="blogText">` + x["posts"]["id" + j] + `<span id="auth"><br><br> - ` + x["author"] + `</span></p><div class="mBlogSidebar"><div class="mBlogSidebar__button"><span class="material-icons" onclick="count('` + j + `')"> favorite_border </span><p id="cval-` + j + `"></p></div><div class="mBlogSidebar__button"><span class="material-icons" onclick="share(`+j+`);"> share</div></div></div><div class="mBlogFooter"><div class="mBlogFooter__ticker"><span class="material-icons mBlogFooter__icon"> account_circle </span><marquee>Designed and Developed by Amlan Saha Kundu, 2022.</marquee></div><a href="https://github.com/yoursamlan" target="_blank"><img src="https://raw.githubusercontent.com/yoursamlan/projX/master/logoonly.png" alt="" class="mBlogFooter__record"/></a></div>`;
+        if (x["showAuth"]) {
+            post.innerHTML = `<div class="blogPost"><p class="blogText">` + x["posts"]["id" + j] + `<span id="auth"><br><br> - ` + x["author"] + `</span></p><div class="mBlogSidebar"><div class="mBlogSidebar__button"><span class="material-icons" onclick="count('` + j + `')"> favorite_border </span><p id="cval-` + j + `"></p></div><div class="mBlogSidebar__button"><span class="material-icons" onclick="share(` + j + `);"> share</div></div></div><div class="mBlogFooter"><div class="mBlogFooter__ticker"><span class="material-icons mBlogFooter__icon"> account_circle </span><marquee>Designed and Developed by Amlan Saha Kundu, 2022.</marquee></div><a href="https://github.com/yoursamlan" target="_blank"><img src="https://raw.githubusercontent.com/yoursamlan/projX/master/logoonly.png" alt="" class="mBlogFooter__record"/></a></div>`;
         } else {
-            post.innerHTML = `<div class="blogPost"><p class="blogText">` + x["posts"]["id" + j] +`</p><div class="mBlogSidebar"><div class="mBlogSidebar__button"><span class="material-icons" onclick="count('` + j + `')"> favorite_border </span><p id="cval-` + j + `"></p></div><div class="mBlogSidebar__button"><span class="material-icons" onclick="share(`+j+`);"> share</div></div></div><div class="mBlogFooter"><div class="mBlogFooter__ticker"><span class="material-icons mBlogFooter__icon"> account_circle </span><marquee>Designed and Developed by Amlan Saha Kundu, 2022.</marquee></div><a href="https://github.com/yoursamlan" target="_blank"><img src="https://raw.githubusercontent.com/yoursamlan/projX/master/logoonly.png" alt="" class="mBlogFooter__record"/></a></div>`;
+            post.innerHTML = `<div class="blogPost"><p class="blogText">` + x["posts"]["id" + j] + `</p><div class="mBlogSidebar"><div class="mBlogSidebar__button"><span class="material-icons" onclick="count('` + j + `')"> favorite_border </span><p id="cval-` + j + `"></p></div><div class="mBlogSidebar__button"><span class="material-icons" onclick="share(` + j + `);"> share</div></div></div><div class="mBlogFooter"><div class="mBlogFooter__ticker"><span class="material-icons mBlogFooter__icon"> account_circle </span><marquee>Designed and Developed by Amlan Saha Kundu, 2022.</marquee></div><a href="https://github.com/yoursamlan" target="_blank"><img src="https://raw.githubusercontent.com/yoursamlan/projX/master/logoonly.png" alt="" class="mBlogFooter__record"/></a></div>`;
             const collection = document.getElementsByClassName("blogText");
             for (let i = 0; i < collection.length; i++) {
                 collection[i].style.top = "35%";
-              }
+            }
         }
         document.getElementById("main").appendChild(post);
     }
@@ -77,13 +80,13 @@ function createPost() {
 }
 
 function share(n) {
-    var y = window.location.href.split("?")[0].replace("/index.html", "")+"?" + n;;
-	try {
-		navigator.clipboard.writeText(y);
-		alert("✔️ Link is Copied ");
-	} catch (err) {
-		console.log("Share Failed", err.message);
-	}
+    var y = window.location.href.split("?")[0].replace("/index.html", "") + "?" + n;;
+    try {
+        navigator.clipboard.writeText(y);
+        alert("✔️ Link is Copied ");
+    } catch (err) {
+        console.log("Share Failed", err.message);
+    }
 }
 
 //=======================================================================================
@@ -93,56 +96,56 @@ api2 = "ff6dd92678060380f6bd";
 var counter = {};
 
 function count(num) {
-	var x = counter["c" + num];
-	x = x + 1;
-	document.getElementById("cval-" + num).textContent = x;
-	counter["c" + num] = x;
-	console.log(counter);
-	push();
+    var x = counter["c" + num];
+    x = x + 1;
+    document.getElementById("cval-" + num).textContent = x;
+    counter["c" + num] = x;
+    console.log(counter);
+    push();
 }
 
 function push() {
-	var x = JSON.stringify(counter);
-	fetch("https://api.npoint.io/" + api1, {
-		body: x,
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json"
-		},
-		method: "POST"
-	}).then((res) => {
-		console.log("Request complete! response:", res);
-		console.log("Push Successful");
-	});
+    var x = JSON.stringify(counter);
+    fetch("https://api.npoint.io/" + api1, {
+        body: x,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST"
+    }).then((res) => {
+        console.log("Request complete! response:", res);
+        console.log("Push Successful");
+    });
 }
 
 function pull() {
-	let URL = "https://api.npoint.io/" + api1;
-	fetch(URL)
-		.then((response) => response.json())
-		.then((data) => {
-			let x = data;
-			console.log(x);
-			window.counter = x;
-			var data = x;
-			Object.keys(data).forEach(function (k) {
-				var d = data[k];
-				for (i = 1; i <= Object.keys(data).length; i++) {
-					try {
-						document.getElementById("cval-" + i).textContent = counter["c" + i];
-					} catch (error) {
-						console.log(error);
-					}
-				}
-			});
+    let URL = "https://api.npoint.io/" + api1;
+    fetch(URL)
+        .then((response) => response.json())
+        .then((data) => {
+            let x = data;
+            console.log(x);
+            window.counter = x;
+            var data = x;
+            Object.keys(data).forEach(function (k) {
+                var d = data[k];
+                for (i = 1; i <= Object.keys(data).length; i++) {
+                    try {
+                        document.getElementById("cval-" + i).textContent = counter["c" + i];
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+            });
 
-			console.log("Pull Successful");
-		})
-		.catch((error) => console.error(error));
+            console.log("Pull Successful");
+        })
+        .catch((error) => console.error(error));
 }
 
 function init() {
     scale();
-	pull();
-	createPost();
+    pull();
+    createPost();
 }
